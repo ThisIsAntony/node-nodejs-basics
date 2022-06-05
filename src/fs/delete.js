a1ccc459b9b2d9dbf6745840936d146c93990041
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-05 14:22:45 +0300

    feat: add file system task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,30 @@
+import fs from "fs";
+import path from "path";
+import { stderr } from "process";
+import { fileURLToPath } from "url";
+import {
+  errorMessage,
+  fileToRemove,
+  FOLDER_FILES,
+} from "../utils/constants/constants.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const currentFile = fileToRemove;
+
 export const remove = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, FOLDER_FILES, currentFile);
+
+  try {
+    await new Promise((res, rej) => {
+      fs.unlink(filePath, (err) => {
+        if (err) rej(new Error(errorMessage));
+        res();
+      });
+    });
+  } catch (e) {
+    stderr.write(e.message);
+  }
 };
+
+remove();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,45 @@
+import fs from "fs";
+import path from "path";
+import { stderr } from "process";
+import { fileURLToPath } from "url";
+import {
+  errorMessage,
+  FOLDER_FILES,
+  properFilename,
+  wrongFilename,
+} from "../utils/constants/constants.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const rename = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, FOLDER_FILES, wrongFilename);
+  const filePathRename = path.join(__dirname, FOLDER_FILES, properFilename);
+
+  try {
+    await new Promise((res, rej) => {
+      fs.access(filePath, fs.F_OK, (err) => {
+        if (err) rej(new Error(errorMessage));
+        res();
+      });
+    });
+
+    await new Promise((res, rej) => {
+      fs.access(filePathRename, fs.F_OK, (err) => {
+        if (!err) rej(new Error(errorMessage));
+        res();
+      });
+    });
+
+    await new Promise((res, rej) => {
+      fs.rename(filePath, filePathRename, (err) => {
+        if (err) rej(new Error(errorMessage));
+        res();
+      });
+    });
+  } catch (e) {
+    stderr.write(e.message);
+  }
 };
+
+rename();
