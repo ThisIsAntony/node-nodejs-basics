221cd62b7f6f03da0dae03b56befe68c85c8bb1f
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-05 14:31:48 +0300

    feat: add worker task

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,38 @@
+import os from "os";
+import path from "path";
+import { fileURLToPath } from "url";
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import { workerFileName } from "../utils/constants/constants.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let num = 10;
+
+const cores = os.cpus().length;
+
 export const performCalculations = async () => {
-    // Write your code here
+  if (isMainThread) {
+    const workers = Array.from({ length: cores }, () => {
+      return new Promise((res) => {
+        const worker = new Worker(path.join(__dirname, workerFileName), {
+          workerData: num++,
+        });
+        worker.on("message", res);
+      });
+    });
+
+    const arrayData = await Promise.allSettled(workers);
+    const resolvedResult = arrayData.map((data) => {
+      const { value, reason } = data;
+      return {
+        status: reason ? "error" : "resolved",
+        data: reason ? null : value,
+      };
+    });
+    console.log(resolvedResult);
+  } else {
+    return parentPort.postMessage(workerData);
+  }
 };
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,20 @@
-// n should be received from main thread
-export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+import { Worker, isMainThread, parentPort, workerData } from "worker_threads";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+export const nthFibonacci = (n) => {
+  if (isMainThread) {
+    const worker = new Worker(__filename, { workerData: n });
+    worker.on("message", console.log);
+  } else {
+    return n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+  }
+};
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (!isMainThread) parentPort.postMessage(nthFibonacci(workerData));
 };
+
+nthFibonacci(10);
+sendResult();
