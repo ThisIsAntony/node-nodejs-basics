83a7e5127fb404d6d5a41df65131d902c151c382
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-05 14:34:01 +0300

    feat: add child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,17 @@
-export const spawnChildProcess = async (args) => {
-    // Write your code here
+import path from "path";
+import { fileURLToPath } from "url";
+import { fork } from "child_process";
+import { stdin, stdout } from "process";
+import { cpScripteFile, FOLDER_FILES } from "../utils/constants/constants.js";
+
+export const spawnChildProcess = async (...args) => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filePath = path.join(__dirname, FOLDER_FILES, cpScripteFile);
+  const childProcess = fork(filePath, args, { silent: true });
+
+  childProcess.stdout.pipe(stdout);
+  stdin.pipe(childProcess.stdin);
 };
+
+spawnChildProcess([3, 1, 4]);
