89cf7fa5ef4bdb35361cd641601be839d8c8ba17
Anatoly Yakushenko <Anatoly.Yakushenko@gmail.com>
2022-06-05 14:30:25 +0300

    feat: add zlip task

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,31 @@
+import zlib from "zlib";
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import {
+  FOLDER_FILES,
+  fileToCompress,
+  archive,
+} from "../utils/constants/constants.js";
+
 export const compress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const readStream = createReadStream(
+    path.join(__dirname, FOLDER_FILES, fileToCompress)
+  );
+
+  const writeStream = createWriteStream(
+    path.join(__dirname, FOLDER_FILES, archive)
+  );
+
+  const compressStream = zlib.createGzip();
+
+  readStream
+    .on("data", (data) => data)
+    .pipe(compressStream)
+    .pipe(writeStream);
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,31 @@
+import zlib from "zlib";
+import path from "path";
+import { createReadStream, createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import {
+  FOLDER_FILES,
+  fileToCompress,
+  archive,
+} from "../utils/constants/constants.js";
+
 export const decompress = async () => {
-    // Write your code here 
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  const readStream = createReadStream(
+    path.join(__dirname, FOLDER_FILES, archive)
+  );
+
+  const writeStream = createWriteStream(
+    path.join(__dirname, FOLDER_FILES, fileToCompress)
+  );
+
+  const compressStream = zlib.createUnzip();
+
+  readStream
+    .on("data", (data) => data)
+    .pipe(compressStream)
+    .pipe(writeStream);
 };
+
+decompress();
